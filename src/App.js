1e10c43436d528824ddaa7846edb4f481a3d9acc
Tristan Guillevin <tristan.guillevin@gmail.com>
2019-09-06 16:35:26 +0200

    Hello World Tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+require("./App.css");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Needed
+const { tableau } = window;
+
+class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { dashboardName: "" };
+  }
+
+  componentDidMount() {
+    tableau.extensions.initializeAsync().then(() => {
+      const dashboardName = tableau.extensions.dashboardContent.dashboard.name;
+
+      this.setState({
+        dashboardName
+      });
+    });
+  }
+
+  render() {
+    return <h1> Hello {this.state.dashboardName}</h1>;
+  }
 }
 
-export default App;
+export default AppComponent;
